f0af32dc2136468e922520234e7cf6017af3bd44
josericardo0 <gmnaarea@gmail.com>
2025-07-11 14:42:49 -0300

    Primeira versao

---
+++ restful-webservice/src/main/java/com/revisao/webservices/restful_webservice/RestfulWebserviceApplication.java
@@ -0,0 +1,15 @@
+package com.revisao.webservices.restful_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"controller", "olamundo"})
+@SpringBootApplication
+public class RestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebserviceApplication.class, args);
+	}
+
+}
